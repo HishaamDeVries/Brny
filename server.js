410d6b30b42eff0a098c8f40746fce3d34def16b
Hishaam De Vries <hishaamdevries@gmail.com>
2021-02-18 12:36:44 +0200

    Commiting to Heroku

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const jsonServer = require('json-server');
+const express = require("express");
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000;
+const app = express(server);
+const path = require("path");
 
-server.use(middlewares);
-server.use(router);
+app.use(middlewares);
+app.use(router);
 
-server.listen(port);
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+  }
+
+app.listen(port);
